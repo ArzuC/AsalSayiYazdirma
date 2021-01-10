d50dc596c2cc5ec33359bfcc069e3f4cd2e9cd70
Arzu <arzucaferoglu@outlook.com>
2021-01-10 16:29:31 +0300

    Add project files.

---
+++ AsalSayiYazdirma/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsalSayiYazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int sayi = 2; sayi <= 10000; sayi++)
+            {
+                bool asalMi = true;
+                for (int i = 2; i < sayi; i++)
+                {
+                    if (sayi % i == 0)
+                    {
+                        asalMi = false;
+                        break;
+                    }
+                }
+                if (asalMi)
+                    Console.Write(" " + sayi);
+            }
+            Console.ReadLine();
+        } 
+        
+    }
+}
